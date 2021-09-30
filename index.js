5591b1dc6f19f40c4ec0ac3c6062fb2704f23077
Muhammad Abdullah <39337161+abdullah249@users.noreply.github.com>
2021-09-30 05:42:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -278,8 +278,8 @@ module.exports = variableName;
 
 })
 
-
-var server = app.listen(8081, function () {
+PORT= process.env.PORT || 8081
+var server = app.listen(PORT, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
