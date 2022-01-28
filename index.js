f841f1583e1f562ab3d3954ca928057bb9d8f513
Muhammad Abdullah <39337161+abdullah249@users.noreply.github.com>
2022-01-28 10:52:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let globalpred='none'
 async function imageapi() {
   
 request
-  .get('https://foodscan.s3.amazonaws.com/Apple_pie_resized.jpg')
+  .get('https://weightchoper.somee.com/StaticFiles/images/Apple_pie_resized.jpg')
   .on('error', function(err,response) {
     console.error(err)
 	console.log(response.statusCode)
